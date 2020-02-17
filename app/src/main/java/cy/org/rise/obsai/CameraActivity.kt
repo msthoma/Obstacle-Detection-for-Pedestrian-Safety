a2a728e8c17772416bdfee2ceef09bb86694f45a
marios <msthoma@yahoo.com>
2020-02-17 10:45:47 +0200

    progress towards saving photo from custom camera view

--- app/src/main/java/cy/org/rise/obsai/CameraActivity.kt
+++ app/src/main/java/cy/org/rise/obsai/CameraActivity.kt
@@ -1,14 +1,24 @@
 package cy.org.rise.obsai
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.CameraView
+import com.otaliastudios.cameraview.FileCallback
 import com.otaliastudios.cameraview.PictureResult
 import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
-class CameraActivity : AppCompatActivity() {
+class CameraActivity : AppCompatActivity(), View.OnClickListener, FileCallback {
     private lateinit var cameraView: CameraView
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +28,47 @@ class CameraActivity : AppCompatActivity() {
         cameraView = camera_view
         cameraView.setLifecycleOwner(this)
         cameraView.addCameraListener(Listener())
+
+        camera_button.setOnClickListener(this)
     }
 
     private class Listener : CameraListener() {
         override fun onPictureTaken(result: PictureResult) {
             super.onPictureTaken(result)
+            Log.d("pictureSize", result.size.toString())
+            Log.d("pictureType", result.javaClass.name)
+//            val photoFile = cr
+//            result.toFile(createImageFile(), FileCallback {})
+        }
+    }
+
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.camera_button -> {
+                Log.d("camera", "button pressed")
+                cameraView.takePicture()
+            }
+        }
+    }
+
+    override fun onFileReady(file: File?) {
+        Log.d("CameraActivity", "file ready")
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}",
+            ".jpg",
+            storageDir
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
         }
     }
 }
