c792f879beaf59aad9ea7c40ef9a86bcefc4da6c
marios <msthoma@yahoo.com>
2020-02-17 10:47:52 +0200

    extension that provides activity's name, for logging purposes

---
+++ app/src/main/java/cy/org/rise/obsai/Extensions.kt
@@ -0,0 +1,8 @@
+package cy.org.rise.obsai
+
+/**
+ * Returns activity name for logging purposes
+ * see https://stackoverflow.com/a/52956934/3755276
+ * also https://stackoverflow.com/a/57123619/3755276
+ */
+inline fun <reified T> T.TAG(): String = T::class.java.simpleName
