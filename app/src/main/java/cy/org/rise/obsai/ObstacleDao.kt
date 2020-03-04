39f585245cfc16ce81ec2e4237f82972559a0c03
marios <msthoma@yahoo.com>
2020-03-04 09:50:07 +0200

    specify OnConflictStrategy

--- app/src/main/java/cy/org/rise/obsai/ObstacleDao.kt
+++ app/src/main/java/cy/org/rise/obsai/ObstacleDao.kt
@@ -3,6 +3,7 @@ package cy.org.rise.obsai
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import io.reactivex.Completable
 
@@ -11,6 +12,6 @@ interface ObstacleDao {
     @Query("SELECT * from obstacle_table ORDER BY obstacle ASC")
     fun getAllObstacles(): LiveData<List<Obstacle>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertObstacle(obstacle: Obstacle): Completable
 }
