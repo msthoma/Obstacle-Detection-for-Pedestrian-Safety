d6bbb15c3404de472d839b769a9c4a71758de886
marios <msthoma@yahoo.com>
2020-03-04 19:48:22 +0200

    specify type converters

--- app/src/main/java/cy/org/rise/obsai/ObstacleRoomDatabase.kt
+++ app/src/main/java/cy/org/rise/obsai/ObstacleRoomDatabase.kt
@@ -4,24 +4,28 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import cy.org.rise.obsai.db.Converters
 
 @Database(entities = [Obstacle::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class ObstacleRoomDatabase : RoomDatabase() {
     abstract fun obstacleDao(): ObstacleDao
 
     companion object {
         // For Singleton instantiation
-        @Volatile private var INSTANCE: ObstacleRoomDatabase? = null
+        @Volatile
+        private var INSTANCE: ObstacleRoomDatabase? = null
 
         fun getInstance(context: Context): ObstacleRoomDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    ObstacleRoomDatabase::class.java,
-                    "obstacle_db"
-                )
+                        context.applicationContext,
+                        ObstacleRoomDatabase::class.java,
+                        "obstacle_db"
+                    )
                     .allowMainThreadQueries()
                     .build()
                 INSTANCE = instance
