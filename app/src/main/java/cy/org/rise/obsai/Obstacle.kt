8b295195104a5de442e74f6de434d1902827aa27
marios <msthoma@yahoo.com>
2020-03-04 14:52:21 +0200

    add fields for type, photo, orientation, altitude

--- app/src/main/java/cy/org/rise/obsai/Obstacle.kt
+++ app/src/main/java/cy/org/rise/obsai/Obstacle.kt
@@ -11,7 +11,18 @@ data class Obstacle(
 
     @ColumnInfo val obstacle: String,
 
+    @ColumnInfo val obs_type: String,
+
+    @ColumnInfo val photo: String,
+
     @ColumnInfo val latitude: Double,
 
-    @ColumnInfo val longitude: Double
+    @ColumnInfo val longitude: Double,
+
+    // Orientation axes (best replaced with class or similar)
+    @ColumnInfo val x: Double,
+    @ColumnInfo val y: Double,
+    @ColumnInfo val z: Double,
+
+    @ColumnInfo val altitude: Double? = null
 )
