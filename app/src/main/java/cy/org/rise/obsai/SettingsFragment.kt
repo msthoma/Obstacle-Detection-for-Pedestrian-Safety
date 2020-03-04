03dc4265edfdfc6f8058c5135b4a15896a0d4e59
marios <msthoma@yahoo.com>
2020-03-04 11:41:50 +0200

    add version field, click listener for libraries activity

--- app/src/main/java/cy/org/rise/obsai/SettingsFragment.kt
+++ app/src/main/java/cy/org/rise/obsai/SettingsFragment.kt
@@ -1,11 +1,23 @@
 package cy.org.rise.obsai
 
-
+import android.content.Intent
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import cy.org.rise.obsai.ui.AboutActivity
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        // set app version
+        findPreference<Preference>("version")?.summary =
+            "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+
+        // click listener for libraries activity
+        findPreference<Preference>("open_source_libraries")?.setOnPreferenceClickListener {
+            startActivity(Intent(activity, AboutActivity::class.java))
+            true
+        }
     }
 }
