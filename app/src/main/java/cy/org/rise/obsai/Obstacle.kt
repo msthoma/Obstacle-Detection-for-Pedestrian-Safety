551c45da19eb69fa08c627f34485fa8a8251435b
marios <msthoma@yahoo.com>
2020-03-04 09:48:12 +0200

    add id and coordinate fields

--- app/src/main/java/cy/org/rise/obsai/Obstacle.kt
+++ app/src/main/java/cy/org/rise/obsai/Obstacle.kt
@@ -3,6 +3,15 @@ package cy.org.rise.obsai
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "obstacle_table")
-data class Obstacle(@PrimaryKey @ColumnInfo(name = "obstacle") val obstacle: String)
+data class Obstacle(
+    @PrimaryKey val id: String = UUID.randomUUID().toString(),
+
+    @ColumnInfo(name = "obstacle") val obstacle: String,
+
+    @ColumnInfo(name = "latitude") val latitude: Double,
+
+    @ColumnInfo(name = "longitude") val longitude: Double
+)
