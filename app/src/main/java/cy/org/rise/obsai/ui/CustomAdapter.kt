8e97df78b6efdc2a5bf19d1e3397223a57d99fb2
marios <msthoma@yahoo.com>
2020-03-06 15:15:20 +0200

    change placeholder, increase row photo resolutions

--- app/src/main/java/cy/org/rise/obsai/ui/CustomAdapter.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/CustomAdapter.kt
@@ -62,9 +62,9 @@ class CustomAdapter internal constructor() :
         // Set picture
         Picasso.get()
             .load(File(obs.photo))
-            .placeholder(R.drawable.ic_noun_barrier_1218014)
-            .resize(120, 120)
-            .centerCrop()
+            .placeholder(R.drawable.ic_noun_barrier_2895012)
+            .resize(200, 0)
+            .centerInside()
             .into(viewHolder.itemView.imageView)
     }
 
