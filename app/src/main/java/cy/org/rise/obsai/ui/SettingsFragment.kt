d488f2c7fa54bede87d4337c2ac156045148401b
marios <msthoma@yahoo.com>
2020-03-09 11:33:33 +0200

    clicking the app version view 5 times will crash the app, to test ACRA crash reporting

--- app/src/main/java/cy/org/rise/obsai/ui/SettingsFragment.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/SettingsFragment.kt
@@ -2,10 +2,12 @@ package cy.org.rise.obsai.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import cy.org.rise.obsai.BuildConfig
 import cy.org.rise.obsai.R
+import cy.org.rise.obsai.utils.TAG
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -14,6 +16,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
         // set app version
         findPreference<Preference>("version")?.summary =
             "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+
+        // intentionally crash app to test crash reporting, triggered by repeated count view clicks
+        var viewClicks = 0
+        findPreference<Preference>("version")?.setOnPreferenceClickListener {
+            viewClicks += 1
+            Log.d(TAG(), "$viewClicks view clicks...")
+            if (viewClicks == 5) {
+                throw RuntimeException("This crash was intentional!")
+            }
+            true
+        }
 
         // click listener for libraries activity
         findPreference<Preference>("open_source_libraries")?.setOnPreferenceClickListener {
