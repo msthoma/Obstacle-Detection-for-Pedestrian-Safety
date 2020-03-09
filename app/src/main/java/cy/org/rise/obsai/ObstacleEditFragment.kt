8470b406496bb1e0786672e63706cb6d8f84d72e
marios <msthoma@yahoo.com>
2020-03-09 09:19:34 +0200

    handle back button presses

--- app/src/main/java/cy/org/rise/obsai/ObstacleEditFragment.kt
+++ app/src/main/java/cy/org/rise/obsai/ObstacleEditFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -121,6 +122,14 @@ class ObstacleEditFragment : Fragment() {
 
             // Set min zoom, so user cannot zoom out too much (1 is world, 20 buildings)
             googleMap.setMinZoomPreference(7.5f)
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            // Handles pressing back in edit fragment, which should go back to the list fragment
+            // instead of the camera
+            findNavController().popBackStack(R.id.obstacleListFragment, false)
+
+            // TODO add discard confirmation dialog here
         }
     }
 
