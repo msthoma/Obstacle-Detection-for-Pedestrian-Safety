8c764a02e5b4846568b0cb7e3462c08a091fb7d6
marios <msthoma@yahoo.com>
2020-03-30 11:08:07 +0300

    add POST insert remote obstacle

--- app/src/main/java/cy/org/rise/obsai/api/OrionService.kt
+++ app/src/main/java/cy/org/rise/obsai/api/OrionService.kt
@@ -1,9 +1,15 @@
 package cy.org.rise.obsai.api
 
 import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface OrionService {
     @GET("version")
     fun getVersion(): Call<OrionVersion>
+
+    @POST("v2/entities")
+    suspend fun insertObstacle(@Body restObstacle: RestObstacle): Response<Unit>
 }
