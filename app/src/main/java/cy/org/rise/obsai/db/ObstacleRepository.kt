891b9e0abe5515fa39d5be3e3746da283b5d9bd6
marios <msthoma@yahoo.com>
2020-03-31 16:05:13 +0300

    add entries for all network methods

--- app/src/main/java/cy/org/rise/obsai/db/ObstacleRepository.kt
+++ app/src/main/java/cy/org/rise/obsai/db/ObstacleRepository.kt
@@ -1,5 +1,7 @@
 package cy.org.rise.obsai.db
 
+import cy.org.rise.obsai.api.FiwareOrionApi
+
 /**
  * Repository module for handling data operations.
  * https://github.com/android/sunflower/blob/master/app/src/main/java/com/google/samples/apps/sunflower/data/PlantRepository.kt
@@ -12,6 +14,17 @@ class ObstacleRepository private constructor(private val obstacleDao: ObstacleDa
     suspend fun insertObstacle(obstacle: Obstacle) = obstacleDao.insertObstacle(obstacle)
 
     suspend fun deleteAll() = obstacleDao.deleteAll()
+
+    // Network operations
+    private val orionService by lazy {
+        FiwareOrionApi.create()
+    }
+
+    suspend fun insertServerObstacle(obstacle: Obstacle) =
+        orionService.insertServerObstacle(obstacle)
+
+    suspend fun getAllServerObstacles(type: String, options: String = "keyValues") =
+        orionService.getAllServerObstacles(type, options)
 
     companion object {
         // For Singleton instantiation
