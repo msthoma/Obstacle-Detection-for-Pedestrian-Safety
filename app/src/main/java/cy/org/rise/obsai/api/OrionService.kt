1d2bc567d22a118ba7aef5c0b90d6580aef94c9d
marios <msthoma@yahoo.com>
2020-03-31 14:19:36 +0300

    add getAllObstacles

--- app/src/main/java/cy/org/rise/obsai/api/OrionService.kt
+++ app/src/main/java/cy/org/rise/obsai/api/OrionService.kt
@@ -6,10 +6,17 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface OrionService {
     @GET("version")
     fun getVersion(): Call<OrionVersion>
+
+    @GET("v2/entities")
+    fun getAllObstacles(
+        @Query("type") type: String,
+        @Query("options") options: String = "keyValues"
+    ): Call<List<Obstacle>>
 
     @POST("v2/entities")
     suspend fun insertObstacle(@Body obstacle: Obstacle): Response<Unit>
