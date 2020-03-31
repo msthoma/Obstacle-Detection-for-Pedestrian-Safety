128db10c9b37decaf9b73f0df10611923c368e9c
marios <msthoma@yahoo.com>
2020-03-31 15:24:06 +0300

    rename to FiwareOrionApi

--- app/src/main/java/cy/org/rise/obsai/api/FiwareOrionApi.kt
+++ app/src/main/java/cy/org/rise/obsai/api/FiwareOrionApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
-interface OrionService {
+interface FiwareOrionApi {
     @GET("version")
     fun getVersion(): Call<OrionVersion>
 
