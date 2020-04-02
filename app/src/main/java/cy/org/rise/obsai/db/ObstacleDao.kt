3808acf9e5a49bfa17b6ee89dd40f83cf45c382b
marios <msthoma@yahoo.com>
2020-04-02 11:53:57 +0300

    adapt query with renamed field

--- app/src/main/java/cy/org/rise/obsai/db/ObstacleDao.kt
+++ app/src/main/java/cy/org/rise/obsai/db/ObstacleDao.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 
 @Dao
 interface ObstacleDao {
-    @Query("SELECT * from obstacle_table ORDER BY time_stamp ASC")
+    @Query("SELECT * from obstacle_table ORDER BY timeStamp ASC")
     fun getAllObstacles(): LiveData<List<Obstacle>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
