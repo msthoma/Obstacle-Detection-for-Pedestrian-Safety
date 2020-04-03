6746be7b8fe9f83f23fd1f7089481d32051a8794
marios <msthoma@yahoo.com>
2020-04-03 11:55:57 +0300

    merged the two classes, removed @SerializedName annotations, since attribute names are the same

--- app/src/main/java/cy/org/rise/obsai/api/OrionVersion.kt
+++ app/src/main/java/cy/org/rise/obsai/api/OrionVersion.kt
@@ -1,7 +1,16 @@
 package cy.org.rise.obsai.api
 
-import com.google.gson.annotations.SerializedName
-
 data class OrionVersion(
-    @SerializedName("orion") val orion: Orion
-)
+    val orion: Orion
+) {
+    data class Orion(
+        val version: String,
+        val uptime: String,
+        val git_hash: String,
+        val compile_time: String,
+        val compiled_by: String,
+        val compiled_in: String,
+        val release_date: String,
+        val doc: String
+    )
+}
