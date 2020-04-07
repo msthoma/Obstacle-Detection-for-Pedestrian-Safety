d0929c507a1800ddefecde597c51fee891c53632
marios <msthoma@yahoo.com>
2020-04-07 10:46:24 +0300

    new class for login responses

---
+++ app/src/main/java/cy/org/rise/obsai/api/LoginResponse.kt
@@ -0,0 +1,17 @@
+package cy.org.rise.obsai.api
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    @SerializedName("access_token")
+    val accessToken: String,
+
+    @SerializedName("expires_in")
+    val expiresIn: Int,
+
+    @SerializedName("refresh_token")
+    val refreshToken: String,
+
+    @SerializedName("token_type")
+    val tokenType: String
+)
