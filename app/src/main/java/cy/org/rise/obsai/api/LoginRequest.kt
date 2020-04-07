4b25b69c6e2987a5fe6c675dddd890eb97b7e382
marios <msthoma@yahoo.com>
2020-04-07 10:45:31 +0300

    new class for login requests

---
+++ app/src/main/java/cy/org/rise/obsai/api/LoginRequest.kt
@@ -0,0 +1,11 @@
+package cy.org.rise.obsai.api
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(
+    @SerializedName("email")
+    var email: String,
+
+    @SerializedName("password")
+    var password: String
+)
