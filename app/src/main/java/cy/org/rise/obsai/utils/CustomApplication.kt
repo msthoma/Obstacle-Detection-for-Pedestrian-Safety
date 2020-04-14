518c0abe5563ca00bcf399f204f8d1c9a30740a3
marios <msthoma@yahoo.com>
2020-04-14 11:24:31 +0300

    move CustomApplication to utils folder

--- app/src/main/java/cy/org/rise/obsai/utils/CustomApplication.kt
+++ app/src/main/java/cy/org/rise/obsai/utils/CustomApplication.kt
@@ -1,9 +1,10 @@
-package cy.org.rise.obsai
+package cy.org.rise.obsai.utils
 
 import android.app.Application
 import android.content.Context
 import android.util.Log
-import cy.org.rise.obsai.utils.TAG
+import cy.org.rise.obsai.BuildConfig
+import cy.org.rise.obsai.R
 import org.acra.ACRA
 import org.acra.annotation.AcraCore
 import org.acra.annotation.AcraDialog
