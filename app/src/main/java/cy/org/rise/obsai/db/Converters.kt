4da9a9adcb1a3184568b3c0f0250cb13aaf22060
marios <msthoma@yahoo.com>
2020-04-14 15:34:24 +0300

    add class and method explanations

--- app/src/main/java/cy/org/rise/obsai/db/Converters.kt
+++ app/src/main/java/cy/org/rise/obsai/db/Converters.kt
@@ -3,12 +3,27 @@ package cy.org.rise.obsai.db
 import androidx.room.TypeConverter
 import java.util.*
 
+/**
+ * Class with Room database [type converters](https://developer.android.com/reference/androidx/room/TypeConverter)
+ */
 class Converters {
+    /**
+     * Converts date in millis to Date.
+     *
+     * @param value date in millis
+     * @return date as Date object
+     */
     @TypeConverter
     fun fromTimestamp(value: Long?): Date? {
         return value?.let { Date(it) }
     }
 
+    /**
+     * Converts Date to millis.
+     *
+     * @param date date as Date
+     * @return date as millis
+     */
     @TypeConverter
     fun dateToTimestamp(date: Date?): Long? {
         return date?.time
