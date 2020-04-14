6939aa7ca65dd7da527ca74e3c7f1de844b25103
marios <msthoma@yahoo.com>
2020-04-14 17:16:00 +0300

    add class documentation

--- app/src/main/java/cy/org/rise/obsai/api/LoginResponse.kt
+++ app/src/main/java/cy/org/rise/obsai/api/LoginResponse.kt
@@ -2,6 +2,15 @@ package cy.org.rise.obsai.api
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Data class for Retrofit login responses.
+ *
+ * @property accessToken token to be used in subsequent connections with server, identifying the
+ * user
+ * @property expiresIn seconds until token expiry
+ * @property refreshToken token that can be used to get a new [accessToken] when it expires
+ * @property tokenType something like "Bearer" or "Basic", depends on the server
+ */
 data class LoginResponse(
     @SerializedName("access_token")
     val accessToken: String,
