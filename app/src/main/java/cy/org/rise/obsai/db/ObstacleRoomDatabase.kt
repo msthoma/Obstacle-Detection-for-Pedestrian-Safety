2356cacaf87269ea0399a3fa014fe51350656fa5
marios <msthoma@yahoo.com>
2020-04-14 16:48:50 +0300

    add class documentation

--- app/src/main/java/cy/org/rise/obsai/db/ObstacleRoomDatabase.kt
+++ app/src/main/java/cy/org/rise/obsai/db/ObstacleRoomDatabase.kt
@@ -6,6 +6,9 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
+/**
+ * Abstract class for Room database.
+ */
 @Database(entities = [Obstacle::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class ObstacleRoomDatabase : RoomDatabase() {
@@ -16,6 +19,12 @@ abstract class ObstacleRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: ObstacleRoomDatabase? = null
 
+        /**
+         * Returns a singleton instance of the app's Room database.
+         *
+         * @param context application context
+         * @return
+         */
         fun getInstance(context: Context): ObstacleRoomDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
