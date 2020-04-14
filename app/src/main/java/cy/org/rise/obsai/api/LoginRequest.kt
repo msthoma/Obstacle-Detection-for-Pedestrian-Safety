834f87b4a14da070367506c31d51d981ca23cbff
marios <msthoma@yahoo.com>
2020-04-14 17:16:11 +0300

    add class documentation

--- app/src/main/java/cy/org/rise/obsai/api/LoginRequest.kt
+++ app/src/main/java/cy/org/rise/obsai/api/LoginRequest.kt
@@ -2,6 +2,12 @@ package cy.org.rise.obsai.api
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Data class for Retrofit login requests.
+ *
+ * @property email user email
+ * @property password user password
+ */
 data class LoginRequest(
     @SerializedName("email")
     var email: String,
