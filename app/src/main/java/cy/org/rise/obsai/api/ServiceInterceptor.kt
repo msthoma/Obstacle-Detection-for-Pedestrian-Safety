dc552d8b779f87b66fe5bef968dad67fa9267b8d
marios <msthoma@yahoo.com>
2020-04-14 16:55:54 +0300

    add class documentation

--- app/src/main/java/cy/org/rise/obsai/api/ServiceInterceptor.kt
+++ app/src/main/java/cy/org/rise/obsai/api/ServiceInterceptor.kt
@@ -2,19 +2,28 @@ package cy.org.rise.obsai.api
 
 import okhttp3.Interceptor
 
-/*
-* Adds an accessToken to the REST request for use authentication
-*
-* Token is only added if it's specified, and if it's needed (not needed for logging in for example)
-*
-* based on this https://stackoverflow.com/a/55651256
-* and this https://stackoverflow.com/a/58333111
-* */
+/**
+ * Adds an accessToken to the REST request for use in authentication.
+ *
+ * Token is only added if it's specified, and if it's needed (not needed for logging in for
+ * example). Based on [this](https://stackoverflow.com/a/55651256)
+ * and [this](https://stackoverflow.com/a/58333111).
+ * */
 
 class ServiceInterceptor : Interceptor {
 
+    /**
+     * AccessToken for authentication.
+     */
     var token: String = ""
 
+    /**
+     * Injects an AccessToken (if one is provided) into a Retrofit request that requires
+     * authentication.
+     *
+     * @param chain
+     * @return
+     */
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
         var request = chain.request()
 
