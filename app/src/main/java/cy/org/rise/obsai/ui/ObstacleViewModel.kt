f08ffe7f3ed9311fa3560d44c7c0ebbe69e69ad8
marios <msthoma@yahoo.com>
2020-04-27 13:11:38 +0300

    remove test for uploading obstacle to server right away

--- app/src/main/java/cy/org/rise/obsai/ui/ObstacleViewModel.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/ObstacleViewModel.kt
@@ -46,13 +46,13 @@ class ObstacleViewModel internal constructor(
             rep.insertObstacle(obstacle)
 
             // push to server
-            try {
-                rep.insertServerObstacle(obstacle.toRestObstacle())
-            } catch (e: MinioException) {
-                // TODO here catch other exceptions as well, e.g. for inserting obstacle to
-                //  Fiware, not only uploading photo to Minio
-                Log.e(TAG(), "photo upload failed $e")
-            }
+//            try {
+//                rep.insertServerObstacle(obstacle.toRestObstacle())
+//            } catch (e: MinioException) {
+//                // TODO here catch other exceptions as well, e.g. for inserting obstacle to
+//                //  Fiware, not only uploading photo to Minio
+//                Log.e(TAG(), "photo upload failed $e")
+//            }
         }
     }
 
