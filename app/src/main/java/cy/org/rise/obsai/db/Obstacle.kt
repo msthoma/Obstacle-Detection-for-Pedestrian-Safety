2533a3a63a2d6e8715a8597b4f218c5703e724ee
marios <msthoma@yahoo.com>
2020-06-22 12:22:02 +0300

    add new field for location as determined by GPS, in case user manually edits the location

--- app/src/main/java/cy/org/rise/obsai/db/Obstacle.kt
+++ app/src/main/java/cy/org/rise/obsai/db/Obstacle.kt
@@ -29,6 +29,8 @@ import java.util.*
  * @property obstacleType type of the obstacle, e.g crack, no pavement etc.
  * @property photoPath where the photo file is located
  * @property location geo coordinates of the obstacle
+ * @property locationFromGPS holds a copy of the GPS-determined location, in case user manually
+ * edits location
  * @property orientation orientation of phone in space when obstacle was recorded
  * @property typeProbabilitiesCNN map of obstacle types with their probabilities as predicted by
  * the convolutional neural network
@@ -57,6 +59,9 @@ data class Obstacle(
     var typeProbabilitiesCNN: Map<String, Float>? = null
 
 ) : Serializable {
+
+    @Embedded(prefix = "GPS_")
+    var locationFromGPS: Location = location.copy()
 
     /**
      * Simple data class to store obstacle location.
