922f5c449df3a4f89e237fb71adb0b2653bb631b
marios <msthoma@yahoo.com>
2020-06-22 15:41:06 +0300

    forgot to declare that setHasOptionsMenu(true)

--- app/src/main/java/cy/org/rise/obsai/ui/CameraFragment.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/CameraFragment.kt
@@ -61,6 +61,7 @@ class CameraFragment : Fragment(), SensorEventListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false)
     }
