d9a522d705ba9f8f7c914feced28fb96ec7d5e20
marios <msthoma@yahoo.com>
2020-06-26 14:39:31 +0300

    add three example intro pages

--- app/src/main/java/cy/org/rise/obsai/ui/AppIntroActivity.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/AppIntroActivity.kt
@@ -1,19 +1,61 @@
 package cy.org.rise.obsai.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroFragment
+import com.github.appintro.AppIntroPageTransformerType
+import cy.org.rise.obsai.R
 
 class AppIntroActivity : AppIntro() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Do NOT call setContentView here
 
+        isWizardMode = true
+//        showStatusBar(false)
+//        setImmersiveMode()
+        isColorTransitionsEnabled = true
+
+        setTransformer(
+            AppIntroPageTransformerType.Parallax(
+                titleParallaxFactor = 1.0,
+                imageParallaxFactor = -1.0,
+                descriptionParallaxFactor = 2.0
+            )
+        )
+
         addSlide(
             AppIntroFragment.newInstance(
                 title = "Welcome",
-                description = "Description"
+                description = "App description",
+                imageDrawable = R.drawable.ic_rise_banner,
+                backgroundColor = resources.getColor(R.color.introColor1)
             )
         )
+
+        addSlide(
+            AppIntroFragment.newInstance(
+                title = "Permissions",
+                description = "Please grant the permissions",
+                imageDrawable = R.drawable.ic_rise_banner,
+                backgroundColor = resources.getColor(R.color.introColor2)
+            )
+        )
+
+        addSlide(
+            AppIntroFragment.newInstance(
+                title = "Privacy policy",
+                description = "Please agree to the policy",
+                imageDrawable = R.drawable.ic_rise_banner,
+                backgroundColor = resources.getColor(R.color.introColor3)
+            )
+        )
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+//        findNavController().navigate(R.id.action_global_obstacleListFragment)
+        finish()
     }
 }
