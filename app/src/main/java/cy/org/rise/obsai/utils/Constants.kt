408a11c91e43bba2261d582104cc0d6e42359ece
marios <msthoma@yahoo.com>
2020-07-01 15:05:07 +0300

    add central Constants file

---
+++ app/src/main/java/cy/org/rise/obsai/utils/Constants.kt
@@ -0,0 +1,9 @@
+package cy.org.rise.obsai.utils
+
+/**
+ * A list of constants used throughout the app.
+ */
+object Constants {
+    // Key for obstacles in Work Manager
+    const val KEY_OBSTACLE_ENTITY = "KEY_OBSTACLE_ENTITY"
+}
