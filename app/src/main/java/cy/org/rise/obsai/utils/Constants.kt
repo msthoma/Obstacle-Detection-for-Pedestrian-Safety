7f1d95e85666a10a28f93649d854ab5410f59313
marios <msthoma@yahoo.com>
2020-07-03 13:02:21 +0300

    add constants for SharedPreferences

--- app/src/main/java/cy/org/rise/obsai/utils/Constants.kt
+++ app/src/main/java/cy/org/rise/obsai/utils/Constants.kt
@@ -6,4 +6,11 @@ package cy.org.rise.obsai.utils
 object Constants {
     // Key for obstacles in Work Manager
     const val KEY_OBSTACLE_JSON = "KEY_OBSTACLE_ENTITY"
+
+    // SharedPreferences file name
+    const val PREFERENCE_FILE_KEY = "cy.org.rise.obsai.PREFERENCE_FILE_KEY"
+
+    // Keys for device unique ID (actually per install unique id)
+    const val PREF_UNIQUE_ID_KEY = "PREF_UNIQUE_ID_KEY"
+    const val PREF_UNIQUE_ID_DEFAULT = "EMPTY"
 }
