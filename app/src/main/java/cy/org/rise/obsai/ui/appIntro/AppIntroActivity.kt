e208828bcc3499aa541ad9b23b4164c342754754
marios <msthoma@yahoo.com>
2020-07-11 12:25:53 +0300

    attempt at handling back button in Intro, needs improvements

--- app/src/main/java/cy/org/rise/obsai/ui/appIntro/AppIntroActivity.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/appIntro/AppIntroActivity.kt
@@ -1,10 +1,13 @@
 package cy.org.rise.obsai.ui.appIntro
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroPageTransformerType
 import cy.org.rise.obsai.R
+import cy.org.rise.obsai.utils.TAG
+import cy.org.rise.obsai.utils.introStatus
 
 /**
  * Activity for showing an intro tutorial when the app is launched for the first time.
@@ -17,7 +20,7 @@ class AppIntroActivity : AppIntro() {
         // Do NOT call setContentView here
 
         // TODO figure out back button
-//        isSystemBackButtonLocked = true
+        isSystemBackButtonLocked = true
 
         isWizardMode = true
 //        showStatusBar(true)
@@ -53,8 +56,27 @@ class AppIntroActivity : AppIntro() {
         )
     }
 
+    override fun onSlideChanged(oldFragment: Fragment?, newFragment: Fragment?) {
+        super.onSlideChanged(oldFragment, newFragment)
+        Log.d("onSlideChanged", newFragment?.tag.toString())
+
+    }
+
+    override fun onNextPressed(currentFragment: Fragment?) {
+        super.onNextPressed(currentFragment)
+        Log.d("onNextPressed", "${currentFragment?.tag}")
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Log.d(TAG(), "back pressed")
+        // TODO: 11/07/20 better way to handle back presses
+        if (applicationContext.introStatus()) finish() else this.finishAffinity()
+    }
+
     override fun onDonePressed(currentFragment: Fragment?) {
         super.onDonePressed(currentFragment)
+        applicationContext.introStatus(setToShown = true)
         // TODO figure out navigation
 //        findNavController().navigate(R.id.action_global_obstacleListFragment)
         finish()
