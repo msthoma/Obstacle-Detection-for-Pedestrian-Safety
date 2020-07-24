caaa2c54303d5f8ae4b485b44931441339388fd6
marios <msthoma@yahoo.com>
2020-07-24 16:21:10 +0300

    add missing Observer import

--- app/src/main/java/cy/org/rise/obsai/ui/CameraFragment.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/CameraFragment.kt
@@ -9,6 +9,7 @@ import android.view.*
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.afollestad.assent.Permission
 import com.afollestad.assent.isAllGranted
@@ -99,7 +100,7 @@ class CameraFragment : Fragment() {
         viewModel.apply {
             // Get last known location and subscribe to location updates
             if (isAllGranted(Permission.ACCESS_FINE_LOCATION)) {
-                locationLiveData.observe(viewLifecycleOwner, androidx.lifecycle.Observer { newLoc ->
+                locationLiveData.observe(viewLifecycleOwner, Observer { newLoc ->
                     // location in some cases can be NULL see
                     // https://developer.android.com/training/location/retrieve-current#last-known
                     currentLocation = newLoc
@@ -118,7 +119,7 @@ class CameraFragment : Fragment() {
             }
 
             // Track orientation
-            orientationLiveData.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            orientationLiveData.observe(viewLifecycleOwner, Observer {
                 accelerometerReading = it.accelerometer
                 magnetometerReading = it.compass
                 orientationAngles = it.orientation
