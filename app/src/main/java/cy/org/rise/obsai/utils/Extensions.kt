1377806c600b754bc6d491b97a230c499523e48d
marios <msthoma@yahoo.com>
2020-07-28 17:26:00 +0300

    add Suppress annotation for TAG() function

--- app/src/main/java/cy/org/rise/obsai/utils/Extensions.kt
+++ app/src/main/java/cy/org/rise/obsai/utils/Extensions.kt
@@ -14,6 +14,7 @@ import java.util.*
  *
  * @param msg string for displaying next to current class name
  */
+@Suppress("unused")
 inline fun <reified T> T.TAG(msg: String = "") =
     T::class.java.simpleName + if (msg.isNotEmpty()) " ($msg)" else ""
 
