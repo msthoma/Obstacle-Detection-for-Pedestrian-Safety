e01631fbcc4e5ae3f990613455a76281258f8c25
marios <msthoma@yahoo.com>
2020-07-31 12:26:34 +0300

    pass only obstacle id to worker instead of obstacle in JSON form, and retrieve it straight from the db when ready to upload

--- app/src/main/java/cy/org/rise/obsai/api/iNicosiaWorker.kt
+++ app/src/main/java/cy/org/rise/obsai/api/iNicosiaWorker.kt
@@ -4,37 +4,40 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.google.gson.Gson
-import cy.org.rise.obsai.db.Obstacle
+import cy.org.rise.obsai.db.ObstacleRoomDatabase
 import cy.org.rise.obsai.utils.Constants
 import cy.org.rise.obsai.utils.TAG
 
-class iNicosiaWorker(appContext: Context, workerParams: WorkerParameters) :
-    Worker(appContext, workerParams) {
+class iNicosiaWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
 
     override fun doWork(): Result {
-        var obstacleJson: String? = null
-        return try {
-            // Get obstacle JSON
-            obstacleJson = inputData.getString(Constants.KEY_OBSTACLE_JSON)
+        var obstacleId: String? = null
 
-            val api = FiwareOrionApi.create(FiwareOrionApi.iNICOSIA_BASE_URL)
+        return try {
+            // Get obstacle ID
+            obstacleId = inputData.getString(Constants.KEY_OBSTACLE_JSON)
 
             // Convert back to Obstacle entity
-            // TODO fix conversion, or find way to pass obstacle class straight to worker
-            val obstacle = Gson().fromJson<Obstacle>(obstacleJson, Obstacle::class.java)
+//            val obstacle = Gson().fromJson<Obstacle>(obstacleId, Obstacle::class.java)
 
-            val response = api?.postToiNicosiaJson(obstacle)?.execute()
+            obstacleId?.let {
+                val dbObstacle = ObstacleRoomDatabase.getInstance(applicationContext).obstacleDao()
+                    .getObstacleById(it)
 
-            return response?.let {
-                if (!response.isSuccessful) {
-                    Result.failure()
-                } else {
-                    Result.success() // TODO pass on Response from API Result.success(....)
-                }
+                val api = FiwareOrionApi.create(FiwareOrionApi.iNICOSIA_BASE_URL)
+
+                val response = api?.postToiNicosiaJson(dbObstacle)?.execute()
+
+                return response?.let {
+                    if (!response.isSuccessful) {
+                        Result.failure()
+                    } else {
+                        Result.success() // TODO pass on Response from API Result.success(....)
+                    }
+                } ?: Result.failure()
             } ?: Result.failure()
         } catch (e: Exception) {
-            Log.e(TAG(), "Failed to upload entity with JSON $obstacleJson", e)
+            Log.e(TAG(), "Failed to upload entity with ID $obstacleId", e)
             Result.failure() // TODO pass on Response from API Result.failure(....)
         }
     }
