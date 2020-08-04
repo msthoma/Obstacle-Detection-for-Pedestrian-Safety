1ee7d0214d7fbcb58d22e582e0b11822236055f6
marios <msthoma@yahoo.com>
2020-08-04 12:07:29 +0300

    get obstacles from server and add them to the map

--- app/src/main/java/cy/org/rise/obsai/ui/MapFragment.kt
+++ app/src/main/java/cy/org/rise/obsai/ui/MapFragment.kt
@@ -1,34 +1,47 @@
 package cy.org.rise.obsai.ui
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import cy.org.rise.obsai.R
+import cy.org.rise.obsai.utils.InjectorUtils
+import cy.org.rise.obsai.utils.TAG
 
 class MapFragment : Fragment() {
+
+    private val viewModel: ObstacleViewModel by viewModels {
+        InjectorUtils.provideObstacleViewModelFactory(this)
+    }
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
          * This callback is triggered when the map is ready to be used.
          * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
          * If Google Play services is not installed on the device, the user will be prompted to
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val NICOSIA_CENTER = LatLng(35.169933, 33.361071)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NICOSIA_CENTER, 12f))
+        viewModel.allServerObstacles.observe(viewLifecycleOwner, Observer { obstacles ->
+            Log.d(TAG(), "got ${obstacles?.size} obstacles")
+            Log.d(TAG(), "obstacle 1: ${obstacles?.get(0)}")
+            obstacles?.forEach {
+                Log.d(TAG(), "adding obstacle at ${it.getLocationAsLatLong()}")
+                googleMap.addMarker(MarkerOptions().position(it.getLocationAsLatLong()))
+            }
+        })
     }
 
     override fun onCreateView(
